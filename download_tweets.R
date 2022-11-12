5ffeed140ee7b877ad64bcf40ae897de62304d53
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2022-11-12 12:52:51 +0100

    script to gather Hertie's following users' tweets

---
+++ download_tweets.R
@@ -0,0 +1,27 @@
+
+usernames_following_data <- readRDS("usernames_following_data.rds")
+#
+# results1 <- rtweet::get_timeline(
+#   user = usernames_following_data$screen_name
+# )
+
+results <- usernames_following_data$screen_name[1:100] |>
+  rtweet::get_timeline(verbose = TRUE)
+
+beepr::beep()
+
+results2 <- usernames_following_data$screen_name[101:300] |>
+  rtweet::get_timeline(verbose = TRUE)
+
+beepr::beep()
+
+results3 <- usernames_following_data$screen_name[301:600] |>
+  rtweet::get_timeline(verbose = TRUE)
+
+beepr::beep()
+
+results4 <- usernames_following_data$screen_name[601:811] |>
+  rtweet::get_timeline(verbose = TRUE)
+
+beepr::beep(sound = 4)
+
