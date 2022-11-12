b95f3c1ea2652c201344f94836fc10f941f2ce78
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2022-11-12 15:19:55 +0100

    adjusts do download tweets

--- download_tweets.R
+++ download_tweets.R
@@ -8,48 +8,69 @@ usernames_following_data <- readRDS("data-raw/usernames_following_data.rds")
 results <- usernames_following_data$screen_name[1:100] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results, "data-raw/results.rds")
+readr::write_rds(results, "data-raw/results.rds", compress = "xz")
 
 beepr::beep()
 
 results2 <- usernames_following_data$screen_name[101:300] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results2, "data-raw/results_2.rds")
+readr::write_rds(results2, "data-raw/results_2.rds", compress = "xz")
 
 beepr::beep()
 
 results3 <- usernames_following_data$screen_name[301:400] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results3, "data-raw/results_3.rds")
+readr::write_rds(results3, "data-raw/results_3.rds", compress = "xz")
 
 beepr::beep()
 
 results4 <- usernames_following_data$screen_name[401:500] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results4, "data-raw/results_4.rds")
+readr::write_rds(results4, "data-raw/results_4.rds", compress = "xz")
 
 beepr::beep()
 
-results5 <- usernames_following_data$screen_name[501:600] |>
-  rtweet::get_timeline(verbose = TRUE)
+# results5 <- usernames_following_data$screen_name[501:600] |>
+#   rtweet::get_timeline(verbose = TRUE)
+#
+#
+# usernames_following_data$screen_name[501:600] |>
+#   purrr::map_df()
 
-readr::write_rds(results5, "data-raw/results_5.rds")
+results5a <- purrr::map(usernames_following_data$screen_name[501:600],
+                       ~purrr::safely(rtweet::get_timeline)())
+
+resultsba <- results5a |>
+  purrr::map_df(
+    .f = ~{.x |>
+        purrr::pluck("result")}
+  )
+
+readr::write_rds(resultsba, "data-raw/results_5.rds", compress = "xz")
 
 beepr::beep()
 
 results6 <- usernames_following_data$screen_name[601:700] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results6, "data-raw/results_6.rds")
+readr::write_rds(results6, "data-raw/results_6.rds", compress = "xz")
 
 results7 <- usernames_following_data$screen_name[701:811] |>
   rtweet::get_timeline(verbose = TRUE)
 
-readr::write_rds(results6, "data-raw/results_7.rds")
+readr::write_rds(results6, "data-raw/results_7.rds", compress = "xz")
 
 beepr::beep(sound = 4)
 
-# dplyr::bind_rows(r)
+
+# bind rows ---------------------------------------------------------------
+
+fs::dir_info(path = "data-raw", regexp = "results") |>
+  dplyr::pull(path) |>
+  purrr::map_df(~{print(.x)
+    readr::read_rds(.x)})
+
+
