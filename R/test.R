c412d17436a55976b09dc3c7eddf929fa4b69999
Radwa-Radwan <112707526+Radwa-Radwan@users.noreply.github.com>
2022-11-15 22:05:24 +0200

    edits_1

--- R/test.R
+++ R/test.R
@@ -127,4 +127,12 @@ all_tweets <- all_tweets |>
 all_tweets |>
   dplyr::filter(is.na(username))
 
+# subset for a while
+df_edges <- df_edges |>
+  head(1000)
+
+nodes_main <- nodes_main |>
+  dplyr::filter(username %in% c(unique(df_edges$from),
+                                unique(df_edges$to)))
+
 
--- tidygraph_plot.R
+++ tidygraph_plot.R
@@ -1,9 +1,10 @@
 library(tidygraph)
 library(ggraph)
+library(tidyverse)
 
 
-graph_tweets <- tbl_graph(nodes = head(nodes_main, n=1000),
-                          edges = head(df_edges),
+graph_tweets <- tbl_graph(nodes = nodes_main,
+                          edges = df_edges,
                           directed = F)
 
 graph_tweets <- graph_tweets %>%
@@ -13,3 +14,50 @@ graph_tweets <- graph_tweets %>%
          closeness = centrality_closeness(), # Closeness centrality
          eigen = centrality_eigen() # Eigen centrality
   )
+
+network_act_df <- graph_tweets %>%
+  activate(nodes) %>% select(-2:-4)  %>%
+  as_tibble()
+
+pop_username <- as_tibble(
+  network_act_df %>% arrange(-degree) %>% select(username) %>% head(),
+  network_act_df %>% arrange(-between) %>% select(username) %>% head(),
+  network_act_df %>% arrange(-closeness) %>% select(username) %>% head(),
+  network_act_df %>% arrange(-eigen) %>% select(username) %>% head()
+) %>% setNames(c("Degree","Betweenness","Closeness","Eigen"))
+
+pop_username
+
+
+set.seed(123)
+graph_tweets <- graph_tweets %>%
+  activate(nodes) %>%
+  mutate(community = group_louvain()) %>% # clustering
+  activate(edges) %>%
+  filter(!edge_is_loop())
+
+graph_tweets %>%
+  activate(nodes) %>%
+  as.data.frame() %>%
+  count(community)
+
+important_user <- function(data) {
+  name_person <- data %>%
+    as.data.frame() %>%
+    filter(community %in% 1:5) %>%
+    select(-community) %>%
+    pivot_longer(-username, names_to = "measures", values_to = "values") %>%
+    group_by(measures) %>%
+    arrange(desc(values)) %>%
+    slice(1:6) %>%
+    ungroup() %>%
+    distinct(username) %>%
+    pull(username)
+
+  return(name_person)
+}
+
+important_person <-
+  graph_tweets %>%
+  activate(nodes) %>%
+  important_user()
