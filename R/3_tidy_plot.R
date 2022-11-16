fbac87ead9cd55078b8efcd7a4e1311d76d1d3ad
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2022-11-16 22:14:32 +0100

    uploading dolphin data from http://users.dimi.uniud.it/~massimo.franceschet/ns/plugandplay/ggraph/ggraph.html

---
+++ R/3_tidy_plot.R
@@ -0,0 +1,20 @@
+## Hertie School - Master of Data Science for Public Policy
+## Intro to Data Science Workshop 2022
+
+## Theme: Network analysis with {ggraph} and {tidygraph}
+## Instructors: Radwa Abdelsalam (github.com/Radwa-Radwan)
+##              Rodrigo Dornelles (github.com/rfdornelles)
+
+
+# {tidygraph} -------------------------------------------------------------
+
+# build the object
+
+# activate
+
+# analysis
+
+# {ggraph} ----------------------------------------------------------------
+
+#
+
